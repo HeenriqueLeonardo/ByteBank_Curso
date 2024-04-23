1dd99121acdbf790ef967894d55e6963fbb4cf0a
Henrique Leonardo <siverpenguin@gmail.com>
2024-04-23 17:16:22 -0300

    Create ListaDeContasCorrentes.cs

---
+++ Array_Collections_C-aula01/bytebank_ATENDIMENTO/bytebank.Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,16 @@
+﻿using bytebank.Modelos.Conta;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util;
+internal class ContasCorrentes
+{
+    private ContaCorrente[] itens = null;
+    public ContasCorrentes(int tamanhoInicial)
+    {
+        itens = new ContaCorrente[tamanhoInicial =5 ];
+    }
+
+    public void AdicionarContaCorrente(ContaCorrente item)
+    {
+        if 
+    }
+}
