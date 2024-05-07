48ca05e592b0a5cf1173760dd3b663f4a8117e1e
Henrique Leonardo <siverpenguin@gmail.com>
2024-05-07 10:44:55 -0300

    Commit - Implementacao
    Implementacao de ler e decodificar arquivos txt usando um limitador de fluxo de dados

--- ByteBank 2.0/ByteBankIO/Program.cs
+++ ByteBank 2.0/ByteBankIO/Program.cs
@@ -1,7 +1,29 @@
 ﻿using ByteBankIO;
+using System.Text;
+using System.Text.Unicode;
 
 var enderecoDoArquivo = "contas.txt";
+var numeroDeBytesLidos = -1;
+
 
 var fluxoDoArquivo = new FileStream(enderecoDoArquivo,
     FileMode.Open);
 
+var buffer = new byte[1024]; //1KB
+
+
+
+while(numeroDeBytesLidos != 0)
+{
+    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+    EscreverBufffer(buffer);
+}
+EscreverBufffer(buffer);
+
+static void EscreverBufffer(byte[] buffer)
+{
+    var utf8 = new UTF8Encoding();
+    var texto = utf8.GetString(buffer);
+    Console.Write(texto);
+   
+}
---
+++ ByteBank 2.0/Teste/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+var fs = new FileStream("teste.txt", FileMode.Open);
+
+var buffer = new byte[1024];
+var encoding = Encoding.ASCII;
+
+var bytesLidos = fs.Read(buffer, 0, 1024);
+var conteudoArquivo = encoding.GetString(buffer, 0, bytesLidos);
+
+Console.Write(conteudoArquivo);
