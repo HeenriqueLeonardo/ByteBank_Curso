381f8385c4e9170dbf35ba1a0a7ef76e913edbd6
Henrique Leonardo <siverpenguin@gmail.com>
2024-05-07 11:57:24 -0300

    Comitt - implementacao de classe
    Implementacao da classe - Lidando com FileStream

---
+++ ByteBank 2.0/ByteBankIO/LidandoComFileStream.cs
@@ -0,0 +1,40 @@
+﻿using ByteBankIO;
+using System.Text;
+using System.Text.Unicode;
+partial class program {
+    static void LidandoComFileStream()
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo,
+            FileMode.Open))
+        {
+            var buffer = new byte[1024]; //1KB
+
+
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBufffer(buffer, numeroDeBytesLidos);
+                Console.WriteLine($"\nbytes lidos: {numeroDeBytesLidos} ");
+            }
+            fluxoDoArquivo.Close();
+
+        }
+
+
+
+        static void EscreverBufffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = new UTF8Encoding();
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+
+        }
+
+    }
+}
--- ByteBank 2.0/ByteBankIO/Program.cs
+++ ByteBank 2.0/ByteBankIO/Program.cs
@@ -5,25 +5,31 @@ using System.Text.Unicode;
 var enderecoDoArquivo = "contas.txt";
 var numeroDeBytesLidos = -1;
 
-
-var fluxoDoArquivo = new FileStream(enderecoDoArquivo,
-    FileMode.Open);
-
-var buffer = new byte[1024]; //1KB
-
-
-
-while(numeroDeBytesLidos != 0)
+using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo,
+    FileMode.Open))
 {
-    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-    EscreverBufffer(buffer);
-}
-EscreverBufffer(buffer);
+    var buffer = new byte[1024]; //1KB
 
-static void EscreverBufffer(byte[] buffer)
+
+
+    while (numeroDeBytesLidos != 0)
+    {
+        numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+        EscreverBufffer(buffer, numeroDeBytesLidos);
+        Console.WriteLine($"\nbytes lidos: {numeroDeBytesLidos} ");
+    }
+    fluxoDoArquivo.Close();
+
+}
+
+
+
+static void EscreverBufffer(byte[] buffer, int bytesLidos)
 {
     var utf8 = new UTF8Encoding();
-    var texto = utf8.GetString(buffer);
+
+    var texto = utf8.GetString(buffer, 0, bytesLidos);
+
     Console.Write(texto);
    
 }
