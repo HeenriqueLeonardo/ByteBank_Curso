fcb6b31a997c84cb28843e0fc8c0b6a2158770c4
Henrique Leonardo <siverpenguin@gmail.com>
2024-05-08 14:11:50 -0300

    Implementacao
    Implementacao de metodo que transforma string em contaCorrente

--- ByteBank 2.0/ByteBankIO/Program.cs
+++ ByteBank 2.0/ByteBankIO/Program.cs
@@ -1,4 +1,6 @@
-﻿partial class Program
+﻿using ByteBankIO;
+
+partial class Program
 {
     static void Main(String[] args)
     {
@@ -10,12 +12,40 @@
             var leitor = new StreamReader(fluxoDeArquivo);
             while (!leitor.EndOfStream)
             {
-                var line = leitor.ReadLine();
-                Console.WriteLine(line);
+                var linha = leitor.ReadLine();
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+                var msg = $"Conta número - {contaCorrente.Numero}\n" +
+                    $"Agência - {contaCorrente.Agencia}\nSaldo - {contaCorrente.Saldo}\n";
+                Console.WriteLine(msg);
 
 
             }
         }
         Console.ReadKey();
+    }
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        // agencia  numero   saldo   titular
+        // 375      4644     2483.13 Jonata
+
+        var campos = linha.Split(',');
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2];
+        var nomeTitular = campos[3];
+
+        var numeroAgencia = int.Parse(agencia);
+        var numeroConta = int.Parse(numero);
+        var numeroSaldo = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(numeroAgencia, numeroConta);
+        resultado.Depositar(numeroSaldo);
+        resultado.Titular = titular;
+
+        return resultado;
     }
 }
