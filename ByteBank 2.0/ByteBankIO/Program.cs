4106ed467c54a1f018b1edcc5ad21585222821c6
Henrique Leonardo <siverpenguin@gmail.com>
2024-05-08 11:42:43 -0300

    Commit - FileStream

--- ByteBank 2.0/ByteBankIO/Program.cs
+++ ByteBank 2.0/ByteBankIO/Program.cs
@@ -1,6 +1,21 @@
-﻿using System.Threading.Channels;
-
-partial class Program
+﻿partial class Program
 {
+    static void Main(String[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
 
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo,
+            FileMode.Open, FileAccess.Read))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+            while (!leitor.EndOfStream)
+            {
+                var line = leitor.ReadLine();
+                Console.WriteLine(line);
+
+
+            }
+        }
+        Console.ReadKey();
+    }
 }
